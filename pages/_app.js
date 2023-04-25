60398968329d76b6b6a59efaf71e606395fba684
vipul1435 <vipulkumarraj2003@gmail.com>
2023-04-26 01:03:38 +0530

    "add to cart remove from cart clear cart done"

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,66 @@
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
+import { useEffect, useState } from 'react'
 
 export default function App({ Component, pageProps }) {
+  const [Cart, setCart] = useState({})
+  const [subTotal, setsubTotal] = useState(0)
+  const saveCart = (myCart)=>{
+      localStorage.setItem("Cart",JSON.stringify(myCart));
+      let subT=0;
+      let keys = Object.keys(myCart);
+      for(let i=0;i<keys.length;i++){
+        subT+=myCart[keys[i]].price*myCart[keys[i]].qty;
+      }
+      setsubTotal(subT);
+  }
+  useEffect(() => {
+    return () => {
+
+      try{
+        if(localStorage.getItem("Cart")){
+          setCart(JSON.parse(localStorage.getItem("Cart")));
+        }
+      } catch(e){
+        localStorage.clear();
+      }
+      
+    }
+  }, [])
+  
+  const clearCart=()=>{
+    setCart({});
+    saveCart({});
+  }
+  const addtoCart = (itemCode,qty,price,name,size,varient)=>{
+    let myCart= Cart;
+    if(itemCode in Cart){
+      myCart[itemCode].qty = Cart[itemCode].qty+1;
+    } else{
+      myCart[itemCode] = {itemCode,qty:1,price,name,size,varient}
+    }
+    setCart(myCart);
+    saveCart(myCart);
+  }
+  const removefromCart=(itemCode,qty,flag)=>{
+    let myCart= Cart;
+    if(itemCode in Cart){
+      if(flag){
+        myCart[itemCode].qty = Cart[itemCode].qty+qty;
+      } else {
+        myCart[itemCode].qty = Cart[itemCode].qty-qty;
+      }
+    }
+    if(myCart[itemCode]["qty"]<=0){
+      delete myCart[itemCode];
+    }
+    setCart(myCart);
+    saveCart(myCart);
+  }
   return <>
-  <Navbar/>
-  <Component {...pageProps} />
+  <Navbar Cart={Cart} addtoCart={addtoCart} removefromCart={removefromCart} clearCart={clearCart} subTotal={subTotal} />
+  <Component Cart={Cart} addtoCart={addtoCart} removefromCart={removefromCart} clearCart={clearCart} subTotal={subTotal}  {...pageProps} />
   <Footer/>
   </>
 }
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,7 +1,8 @@
 import Checkdelivery from '@/components/Checkdelivery'
 import { useRouter } from 'next/router'
 
-const Post = () => {
+const Post = (props) => {
+  const {addtoCart}=props;
   const router = useRouter()
   const { slug } = router.query
 
@@ -78,7 +79,7 @@ const Post = () => {
         <div className="flex">
           <span className="title-font font-medium text-2xl text-gray-900">$58.00</span>
           <button className="flex items-center ml-7 text-white bg-indigo-500 border-0 py-1 px-4 focus:outline-none hover:bg-indigo-600 rounded">Buy Now</button> 
-          <button className="flex items-center ml-3 text-white bg-indigo-500 border-0 py-1 px-4 focus:outline-none hover:bg-indigo-600 rounded">Add to Cart</button>
+          <button className="flex items-center ml-3 text-white bg-indigo-500 border-0 py-1 px-4 focus:outline-none hover:bg-indigo-600 rounded" onClick={()=>{addtoCart(slug,1,4454,"myoldCAER FUCKING YOU","xl","RED")}}>Add to Cart</button>
           <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
             <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
               <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
